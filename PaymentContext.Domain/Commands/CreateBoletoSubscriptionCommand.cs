13afdc944ea8111e1cf457a6064b7bd92bee7911
Lucas Rodrigues <53006018+lferlr@users.noreply.github.com>
2022-12-22 09:49:21 -0300

    Fail Fast Validations

--- PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreateBoletoSubscriptionCommand.cs
@@ -1,8 +1,10 @@
+using Flunt.Notifications;
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
 
 namespace PaymentContext.Domain.Commands;
 
-public class CreateBoletoSubscriptionCommand
+public class CreateBoletoSubscriptionCommand : Notifiable, ICommand
 {
     // Student
     public string FirstName { get; set; }
@@ -36,4 +38,8 @@ public class CreateBoletoSubscriptionCommand
     public string State { get; set; }
     public string Country { get; set; }
     public string ZipCode { get; set; }
+
+    public void Validate()
+    {
+    }
 }
